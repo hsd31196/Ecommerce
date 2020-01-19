233496f84a6aa465d0e6afaaf037064068b1d632
Harsh Desai <hsd31196@gmail.com>
2020-01-19 19:38:38 +0530

    Adding checkout activity

---
+++ app/src/main/java/com/example/ecommerce/CheckoutDeliveryAddress.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckoutDeliveryAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_delivery_address);
+    }
+}
